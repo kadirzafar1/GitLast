9745a0c07255a099670457d6d124206d77a1e077
Kadir <kadirzafar@gmail.com>
2021-08-31 20:19:40 +0530

    first user commit

---
+++ Alert java.py
@@ -0,0 +1,15 @@
+import time
+
+from selenium import webdriver
+validate = "abdul"
+
+driver = webdriver.Chrome(executable_path="F:\\chromedriver.exe")
+driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+driver.maximize_window()
+driver.find_element_by_css_selector("#name").send_keys("validate")
+driver.find_element_by_id("alertbtn").click()
+time.sleep(3)
+alert = driver.switch_to.alert
+validate = alert.text
+assert validate in alert.text
+alert.accept()
