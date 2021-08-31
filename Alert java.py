8081db019b83fcb771c85aaf130ca151d9424790
Kadir <kadirzafar@gmail.com>
2021-08-31 22:05:41 +0530

    develop branch

--- Alert java.py
+++ Alert java.py
@@ -7,6 +7,11 @@ driver = webdriver.Chrome(executable_path="F:\\chromedriver.exe")
 driver.get("https://rahulshettyacademy.com/AutomationPractice/")
 driver.maximize_window()
 driver.find_element_by_css_selector("#name").send_keys("validate")
+
+driver = webdriver.Chrome(executable_path="F:\\chromedriver.exe")
+driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+driver.maximize_window()
+driver.find_element_by_css_selector("#name").send_keys("validate")
 driver.find_element_by_id("alertbtn").click()
 time.sleep(3)
 alert = driver.switch_to.alert
